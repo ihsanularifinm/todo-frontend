418c382f2f9254d49306cd5f5e08ba5105d7f4ec
zsihsan <72428799+zsihsan@users.noreply.github.com>
2021-06-16 11:05:45 +0700

    tahap 4 - point 1-2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <HelloWorld />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <router-link to="/todo">Halaman Todo</router-link>
+    &nbsp;
+    <router-link to="/user">Halaman User</router-link>
+    <router-view></router-view>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
---
+++ src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div>
+            <h1>Berikut adalah daftar user yang terdaftar</h1>
+        </div>
+        <ul>
+            <li v-for="item in users" :key="item.id">
+                {{ item.username }} <button @click="hapus(item.id)">X</button>
+            </li>
+        </ul>
+        <input v-model="username" />
+        &nbsp;
+        <input v-model="password" />
+        &nbsp;
+        <button @click="tambah">Add</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data: function () {
+        return {
+            users: [],
+            username: '',
+            password: '',
+        };
+    },
+    created: function () {
+        // let username = localStorage.getItem('usr')
+        // let password = localStorage.getItem('pwd')
+        axios.get('http://localhost:3080/user').then((result) => {
+            this.users = result.data;
+        });
+    },
+    methods: {
+        tambah: function () {
+            let newUser = { username: this.username, password: this.password };
+            axios
+                .post('http://localhost:3080/user', newUser)
+                .then((response) => {
+                    this.users.push({
+                        id: response.data.id,
+                        username: this.username,
+                        password: this.password,
+                    });
+                });
+        },
+        hapus: function (id) {
+            axios.delete(`http://localhost:3080/user/${id}`).then(() => {
+                var filtered = this.users.filter((item) => item.id != id);
+                this.users = filtered;
+            });
+        },
+    },
+};
+</script>
